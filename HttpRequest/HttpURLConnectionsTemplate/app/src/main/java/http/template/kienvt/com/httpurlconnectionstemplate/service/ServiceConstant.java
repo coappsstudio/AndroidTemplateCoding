0b9aed48d17c36a4248847485e50f843f656c62e
MrKien <mrkien@MrKiens-MacBook-Pro.local>
2015-09-03 23:59:47 +0700

    created template httpurlconnection

---
+++ HttpRequest/HttpURLConnectionsTemplate/app/src/main/java/http/template/kienvt/com/httpurlconnectionstemplate/service/ServiceConstant.java
@@ -0,0 +1,10 @@
+package http.template.kienvt.com.httpurlconnectionstemplate.service;
+
+/**
+ * Created by mrkien on 9/3/15.
+ */
+public interface ServiceConstant {
+    public boolean isDevelopmentMode = false;
+    public String PREFIX_URL_DEVELOPMENT = "";
+    public String PREFIX_URL_PRODUCTION = "";
+}
